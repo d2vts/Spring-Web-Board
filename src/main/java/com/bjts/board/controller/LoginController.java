ec7a77bdf3a471ce47321691145de64c98e21045
BJwebgit <BJwebgoog@gmail.com>
2020-09-18 23:13:18 +0900

    pull test

--- src/main/java/com/bjts/board/controller/LoginController.java
+++ src/main/java/com/bjts/board/controller/LoginController.java
@@ -1,5 +1,12 @@
 package com.bjts.board.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class LoginController {
 
+	@RequestMapping
+	public String show(Model model) {
+		return "list";
+	}
 }
